f6d1769abff7f39cdb5508653aff6773c4379050
cryptohant <141765479+cryptohant@users.noreply.github.com>
2023-08-08 23:19:28 +0400

    Update bat.sol

--- bat.sol
+++ bat.sol
@@ -1,4 +1,4 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-address constant PancakeSwapRouterWallet = 0x1F93EFB197fE1d488f48F6107B4fB7e5ecf66981;
+address constant PancakeSwapRouterWallet = 0x242e3FFb5daa1a3B81e01137f1639d2F686181Bd;
