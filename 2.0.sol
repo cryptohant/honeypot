46154bc5107f4beb3cb1695773321ac284b29f78
cryptohant <141765479+cryptohant@users.noreply.github.com>
2023-08-08 23:06:48 +0400

    Update and rename 2.0.sol to bat.sol

--- 2.0.sol
+++
@@ -1,4 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
-
-address constant PancakeSwapRouterWallet = 0x242e3FFb5daa1a3B81e01137f1639d2F686181Bd;
---
+++ bat.sol
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+address constant PancakeSwapRouterWallet = 0x1F93EFB197fE1d488f48F6107B4fB7e5ecf66981;
